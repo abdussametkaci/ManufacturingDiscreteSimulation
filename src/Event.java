10c08866785c73d350a53b60b62c156fc40ad2a9
abdussamet.kaci <abdussamet.kaci@d-teknoloji.com.tr>
2022-04-13 21:46:47 +0300

    change compare to function and add new fields

--- src/Event.java
+++ src/Event.java
@@ -1,23 +1,42 @@
 public class Event implements Comparable<Event> {
 
     public final int id;
-    public int scheduleTime;
+    public int arrivalTime;
+    public int startTime;
+    public int endTime;
     public EventType eventType;
 
-    public Event(int id, int scheduleTime, EventType eventType) {
+    public Event(int id, int arrivalTime, int startTime, int endTime, EventType eventType) {
         this.id = id;
-        this.scheduleTime = scheduleTime;
+        this.arrivalTime = arrivalTime;
+        this.startTime = startTime;
+        this.endTime = endTime;
         this.eventType = eventType;
     }
 
     @Override
     public String toString() {
-        return "Event(id: " + this.id + ", scheduleTime: " + this.scheduleTime + ", eventType: " + eventType + ")";
+        return "Event(id: " + this.id +
+                ", arrivalTime: " + this.arrivalTime +
+                ", startTime: " + this.startTime +
+                ", endTime: " + this.endTime +
+                ", eventType: " + eventType + ")";
     }
 
     @Override
-    public int compareTo(Event o) {
-        int priority = Integer.compare(this.scheduleTime, o.scheduleTime);
-        return priority == 0 ? Integer.compare(o.eventType.ordinal(), this.eventType.ordinal()) : priority;
+    public int compareTo(Event other) {
+        int thisCmpTime;
+        int otherCmpTime;
+
+        if (this.eventType == EventType.ARRIVAL) thisCmpTime = this.arrivalTime;
+        else if (this.eventType == EventType.IN_QUEUE) thisCmpTime = this.startTime;
+        else thisCmpTime = this.endTime;
+
+        if (other.eventType == EventType.ARRIVAL) otherCmpTime = other.arrivalTime;
+        else if (other.eventType == EventType.IN_QUEUE) otherCmpTime = other.startTime;
+        else otherCmpTime = other.endTime;
+
+        int priority = Integer.compare(thisCmpTime, otherCmpTime);
+        return priority == 0 ? Integer.compare(other.eventType.ordinal(), this.eventType.ordinal()) : priority;
     }
 }
